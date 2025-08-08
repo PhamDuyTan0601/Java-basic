9cad2bf26f3e580fd870ad0d62f13b3c9e70bffc
PhamDuyTan0601 <phamduytanr@gmail.com>
2025-08-08 13:52:17 +0700

    chore: update hello world message

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Java! Tôi là Duy Tân.");
+        System.out.println("Xin chào! Tôi đang học Java backend.");
+
     }
 }
